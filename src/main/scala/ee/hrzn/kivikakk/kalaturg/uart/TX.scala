7600beb797e45c85984294fc2e2de6d1fdd31a34
Asherah Connor <ashe@kivikakk.ee>
2024-05-04 21:03:10 +0300

    TX: clean, with clean spec.

--- src/main/scala/ee/hrzn/kivikakk/kalaturg/uart/TX.scala
+++ src/main/scala/ee/hrzn/kivikakk/kalaturg/uart/TX.scala
@@ -3,57 +3,42 @@ package ee.hrzn.kivikakk.kalaturg.uart
 import chisel3._
 import chisel3.util._
 
-class TX(val divisor: Int) extends Module {
+class TX(private val divisor: Int) extends Module {
   val io = IO(Flipped(Decoupled(UInt(8.W))))
   val platIo = IO(Output(Bool()))
 
   object State extends ChiselEnum {
-    val sIdle, sSTART, sWaitNext, sSTOP = Value
+    val sIdle, sTx = Value
   }
   private val state = RegInit(State.sIdle)
 
   private val timerReg = Reg(UInt(unsignedBitLength(divisor - 1).W))
-  private val counterReg = Reg(UInt(unsignedBitLength(7).W))
-  private val shiftReg = Reg(UInt(8.W))
+  private val counterReg = Reg(UInt(unsignedBitLength(9).W))
+  private val shiftReg = Reg(UInt(10.W))
 
   platIo := true.B
   io.ready := false.B
 
   switch(state) {
     is(State.sIdle) {
+      io.ready := true.B
       when(io.valid) {
-        timerReg := 0.U
-        shiftReg := io.bits
-        io.ready := true.B
-        state := State.sSTART
+        timerReg := (divisor - 1).U
+        counterReg := 9.U
+        shiftReg := 0.U(1.W) ## io.bits ## 1.U(1.W)
+        state := State.sTx
       }
     }
-    is(State.sSTART) {
-      platIo := false.B
-      timerReg := timerReg + 1.U
-      when(timerReg === (divisor - 1).U) {
-        timerReg := 0.U
-        counterReg := 0.U
-        state := State.sWaitNext
-      }
-    }
-    is(State.sWaitNext) {
-      platIo := shiftReg(7)
-      timerReg := timerReg + 1.U
-      when(timerReg === (divisor - 1).U) {
-        timerReg := 0.U
-        when(counterReg === 7.U) {
-          state := State.sSTOP
-        }.otherwise {
-          counterReg := counterReg + 1.U
-          shiftReg := shiftReg(6, 0) ## 0.U(1.W)
+    is(State.sTx) {
+      platIo := shiftReg(9)
+      timerReg := timerReg - 1.U // TODO: cheaper here or top level?
+      when(timerReg === 0.U) {
+        timerReg := (divisor - 1).U
+        counterReg := counterReg - 1.U
+        shiftReg := shiftReg(8, 0) ## 0.U(1.W)
+        when(counterReg === 0.U) {
+          state := State.sIdle
         }
-      }
-    }
-    is(State.sSTOP) {
-      timerReg := timerReg + 1.U
-      when(timerReg === (divisor - 1).U) {
-        state := State.sIdle
       }
     }
   }
---
+++ src/test/scala/ee/hrzn/kivikakk/kalaturg/uart/TXSpec.scala
@@ -0,0 +1,33 @@
+package ee.hrzn.kivikakk.kalaturg.uart
+
+import chisel3._
+import chiseltest._
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.must.Matchers
+
+class TXSpec extends AnyFlatSpec with ChiselScalatestTester with Matchers {
+  behavior.of("TX")
+
+  it should "transmit a byte" in {
+    test(new TX(divisor = 3)).withAnnotations(Seq(WriteVcdAnnotation))(c => {
+      // Note that poked inputs take effect *immediately* on combinatorial
+      // circuits. We want to poke as the first thing we do in any simulated
+      // cycle, as if responding to the last cycle.  We do this before any
+      // expects -- otherwise we might not observe comb changes correctly.
+      c.io.bits.poke("b10101100".U)
+      c.io.valid.poke(true.B)
+
+      c.platIo.expect(true.B)
+      c.io.ready.expect(true.B)
+
+      c.clock.step()
+
+      c.io.valid.poke(false.B)
+
+      for { bit <- Seq(0, 1, 0, 1, 0, 1, 1, 0, 0, 1); i <- 0 until 3 } {
+        c.platIo.expect((bit == 1).B)
+        c.clock.step()
+      }
+    })
+  }
+}
