a20f7591e57cf1dd0361f0af4a166d8bb418ad97
Asherah Connor <ashe@kivikakk.ee>
2024-05-05 13:05:21 +0300

    UARTSpec: rewonk.

--- src/test/scala/ee/hrzn/kivikakk/kalaturg/uart/UARTSpec.scala
+++ src/test/scala/ee/hrzn/kivikakk/kalaturg/uart/UARTSpec.scala
@@ -9,67 +9,74 @@ import org.scalatest.matchers.must.Matchers
 class UARTSpec extends AnyFlatSpec with ChiselScalatestTester with Matchers {
   behavior.of("UART")
 
-  // TODO: We have our cleaned up RX and TX modules now, and concise specs for
-  //  each. UART now just needs to become the component which manages queueing
-  //  the data in and out, per
-  //  https://hrzn.ee/kivikakk/kalaturg/src/commit/e2d3cff/kalaturg/rtl/uart.py.
+  // These tests are *really* ugly, but they work for now. Need more clarity.
 
   it should "receive a byte" in {
-    test(new UART(baud = 1, clockHz = 5)).withAnnotations(Seq(WriteVcdAnnotation))(c => {
-      c.platIo.rx.poke(true.B)
-      c.rxIo.valid.expect(false.B)
-
+    test(new UART(baud = 1, clockHz = 3)).withAnnotations(Seq(WriteVcdAnnotation))(c => {
       // Assert START and hold for one bit.
       c.platIo.rx.poke(false.B)
-      c.clock.step(5)
+
+      c.rxIo.valid.expect(false.B)
+
+      c.clock.step(3)
 
       // Generate a byte and play it out. Ensure we remain not `rdy`.
       val input = (new scala.util.Random).nextInt(256)
-      for (bitIx <- 7 to 0 by -1) {
-        c.rxIo.valid.expect(false.B)
+      for (bitIx <- 7 to 0 by -1 ; i <- 0 until 3) {
         c.platIo.rx.poke(((input & (1 << bitIx)) != 0).B)
-        c.clock.step(5)
+        c.rxIo.valid.expect(false.B)
+        c.clock.step()
       }
 
-      // Assert STOP and hold for one bit.
-      c.rxIo.valid.expect(false.B)
+      // Assert STOP and hold for one bit; wait for sync and processing delay (?).
       c.platIo.rx.poke(true.B)
-      c.clock.step(5)
+
+      for { i <- 0 until 7 } {
+        c.rxIo.valid.expect(false.B)
+        c.clock.step()
+      }
 
       // Check received OK.
-      c.rxIo.valid.expect(true.B)
-      c.rxIo.bits.expect(input)
-
-      // Ensure we can move to the next byte. (TODO: actually have a FIFO.)
       c.rxIo.ready.poke(true.B)
+
+      c.rxIo.valid.expect(true.B)
+      c.rxIo.bits.byte.expect(input)
+
+      // Ensure we can move to the next byte.
       c.clock.step()
+      c.rxIo.ready.poke(false.B)
       c.rxIo.valid.expect(false.B)
     })
   }
 
   it should "transmit a byte" in {
-    test(new UART(baud = 1, clockHz = 5)).withAnnotations(Seq(WriteVcdAnnotation))(c => {
-      c.platIo.tx.expect(true.B)
-
+    test(new UART(baud = 1, clockHz = 3)).withAnnotations(Seq(WriteVcdAnnotation))(c => {
       // Generate a byte and request it to be sent.
       val input = (new scala.util.Random).nextInt(256)
       c.txIo.bits.poke(input.U)
       c.txIo.valid.poke(true.B)
 
+      c.platIo.tx.expect(true.B)
+
+      c.clock.step()
+      c.txIo.valid.poke(false.B)
+
+      c.platIo.tx.expect(true.B)
+
       // Watch START.
-      for { i <- 0 until 5 } {
+      for { i <- 0 until 3 } {
         c.clock.step()
         c.platIo.tx.expect(false.B)
       }
 
       // Check for each bit in turn.
-      for { bitIx <- 7 to 0 by -1; i <- 0 until 5 } {
+      for { bitIx <- 7 to 0 by -1 ; i <- 0 until 3 } {
         c.clock.step()
         c.platIo.tx.expect(((input & (1 << bitIx)) != 0).B)
       }
 
       // Watch STOP.
-      for { i <- 0 until 5 } {
+      for { i <- 0 until 3 } {
         c.clock.step()
         c.platIo.tx.expect(true.B)
       }
