174a4653d923f2a4da293b9f4a3e413e01e181c8
Asherah Connor <ashe@kivikakk.ee>
2024-05-16 13:54:05 +0300

    adjust blackbox.

--- src/main/scala/ee/kivikakk/kalaturg/CXXRTLTestbench.scala
+++ src/main/scala/ee/kivikakk/kalaturg/CXXRTLTestbench.scala
@@ -1,9 +1,8 @@
 package ee.kivikakk.kalaturg
 
 import chisel3._
-import ee.hrzn.chryse.platform.cxxrtl.CXXRTLBlackBox
 
-class CXXRTLTestbench extends CXXRTLBlackBox {
+class CXXRTLTestbench extends BlackBox {
   val io = IO(new Bundle {
     val clock = Input(Clock())
     val rx    = Input(Bool())
